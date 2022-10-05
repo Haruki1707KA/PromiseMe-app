c41472a03b4ff45a7a997cfa4975342c220e9371
Aiblb <114314506+Aiblb@users.noreply.github.com>
2022-10-05 16:02:43 -0600

    Comment

--- database/migrations/2022_10_05_215348_create_promises_table.php
+++ database/migrations/2022_10_05_215348_create_promises_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
-            $table->foreingId('user_id');
+            //$table->foreingId('user_id');
             $table->timestamps();
         });
     }
