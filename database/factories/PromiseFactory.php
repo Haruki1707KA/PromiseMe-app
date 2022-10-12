46efb4289e2ecaee72c59d8eabde2cb21a83bddc
feo <marvitho777@gmail.com>
2022-10-12 15:36:50 -0600

    Select promises

---
+++ database/factories/PromiseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Promise>
+ */
+class PromiseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'public' => fake()->boolean(),
+            'user_id' => User::factory(),
+            //
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Promise;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', function () {
+    return view('users', [
+        'users' => User::all()
+    ]);
+
+});
+
+Route::get('/promises', function () {
+    return view('promises', [
+        'promises' => Promise::all()->where('public', '=', '1')
+    ]);
+
+});
+
+
+Route::get('/login', function (){
+    return view('login');
+});
+
+Route::post('/login', function (){
+    $data = request();
+});
+
